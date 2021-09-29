03908198cbc10cd415687cfc1078589c265fc225
Sofia Tejada <sofianais@icloud.com>
2021-09-28 18:32:44 -0700

    i mocked stuff

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -3,9 +3,22 @@ import { render, screen } from '@testing-library/react';
 import CharacterList from '../characters/CharacterList';
 import Character from '../characters/Character';
 import { MemoryRouter } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import mock from './mock.json';
 
+const server = setupServer(
+  // eslint-disable-next-line max-len
+  rest.get('https://hey-arnold-api.herokuapp.com/api/v1/characters/', 
+    (req, res, ctx) => {
+      return res(
+        ctx.json(mock)
+      );
+    })
+);
 
 describe('character component', () => {
+
   it('displays a character', () => {
     render(
       <Character 
@@ -20,14 +33,20 @@ describe('character component', () => {
 });
 
 describe('character list component', () => {
+
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   it('displays a list of characters', async () => {
     render(
       <MemoryRouter>
         <CharacterList />
       </MemoryRouter>
     );
-
+    
+    screen.getByAltText('loading gif');
     const list = await screen.findByLabelText('character-list');
+    screen.debug();
     expect(list).toMatchSnapshot();
   });
 });
